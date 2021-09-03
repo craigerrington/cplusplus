4912e2601f1affc0947a30aa465a096db16c5c97
Craig Errington <craigerrington@gmail.com>
2021-09-03 22:15:17 +0100

    variables and combo output

---
+++ variables/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int width;
+    width = 5; // copy assignment of value 5 into variable width
+
+    // variable width now has value 5
+
+    width = 7; // change value stored in variable width to 7
+
+    // variable width now has value 7
+
+    std::cout << "The width is " << width; // ah ha
+
+    return 0;
+}
